f9a00d53e05288c5a069da890522045b77ca0d94
JustKiddingCLK <jonathanking112103@gmail.com>
2023-04-14 16:56:02 -0400

    First Commit

--- Homework/Homework 4/analysis.c
+++ Homework/Homework 4/analysis.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
-#include "header.h"
+
 #define ITEMS 51
 
-int main(void)
-{
+void get_totals(int[][7], int, int);
+
+void get_profits(int[][7], int, int, float[][2]);
+
+void top_earner(int[][7], int, int, float[][2]);
+
+
+int main(void){
+
     // allocate arrays to store 6 months of data
     int month_data[ITEMS][7];
     float money[ITEMS][2];
 
     // initialize money and month_data to zeros
-    for (int i = 0; i < 7; i ++)
-    {
-        for (int j = 0; j < ITEMS; j++)
-        {
+    for (int i = 0; i < 7; i ++){
+        for (int j = 0; j < ITEMS; j++){
             month_data[j][i] = 0;
         }
     }
-    for (int i = 0; i < ITEMS; i++)
-    {
+    for (int i = 0; i < ITEMS; i++){
         money[i][0] = 0;
         money[i][1] = 0;
     }
@@ -26,12 +30,10 @@ int main(void)
     float cost;
 
     //loop over all months
-    for (int i = 0; i < 6; i++)
-    {
+    for (int i = 0; i < 6; i++){
         FILE* f1;
         // open sales file
-        switch (i)
-        {
+        switch (i) {
             case 0:
                 f1 = fopen("Jan_data.txt", "r");
                 break;
@@ -50,20 +52,24 @@ int main(void)
             case 5:
                 f1 = fopen("Jun_data.txt", "r");
                 break;
-        }
+        } // end switch
+
+        // load sales data
+
 
         while(fscanf(f1, "%d %f", &new_item, &cost) > 0){
             month_data[new_item][i+1] = month_data[new_item][i+1] + 1;
             month_data[new_item][0] = new_item;
         }
         fclose(f1);
-    }
 
+    } // end loop over months
+
+    // load sales data
     FILE* f2 = fopen("Costs.txt", "r");
     float purchase;
 
-    while(fscanf(f2, "%i %f %f", &new_item, &purchase, &cost) > 0)
-    {
+    while(fscanf(f2, "%i %f %f", &new_item, &purchase, &cost) > 0){
         money[new_item][0] = purchase;
         money[new_item][1] = cost;
     }
@@ -72,5 +78,5 @@ int main(void)
     get_totals(month_data, ITEMS, 7);
     get_profits(month_data, ITEMS, 7, money);
     top_earner(month_data, ITEMS, 7, money);
-
+    
 }
--- Homework/Homework 4/functions.c
+++ Homework/Homework 4/functions.c
@@ -21,14 +21,18 @@ void get_profits(int month_data[][7], int num_items, int num_months, float money
     FILE* f = fopen("Profits.txt", "w");
     float total_income, total_cost, profit;
     fprintf(f, "Item\t#\tIncome\tCost\tProfit\n");
-    for (int i = 1; i <= num_items; i++) {
+    for (int i = 1; i <= num_items; i++)
+    {
         total_income = 0;
-        total_cost = money[i][0];
-        for (int j = 1; j <= num_months; j++) {
+        total_cost = money[i][1];
+        float profits[num_items];
+        int max_index = i;
+        for (int j = 1; j <= num_months; j++)
+        {
             total_income += month_data[i][j] * money[i][1];
         }
         profit = (total_income - total_cost) * month_data[i][1];
-        fprintf(f, "%02d\t%.2f\t%.2f\t%.2f\n", i, total_income, total_cost, profit);
+        fprintf(f, "%02d\t%02d\t%.2f\t%.2f\t%.2f\n", i, month_data[i+1][0], total_income, total_cost, profit);
     }
     fclose(f);
 }
@@ -60,7 +64,7 @@ void top_earner(int month_data[][7], int num_items, int num_months, float money[
             month_data[max_index+1][0] = temp_item;
         }
         if (i < 10) {
-            fprintf(f, "%02d\t%.2f\n", month_data[i+1][0], profits[i]);
+            fprintf(f, "%02d\t%02d\t%.2f\n", i+1, month_data[i+1][0], profits[i]);
         } else {
             break;
         }
